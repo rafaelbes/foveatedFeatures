86cba0a662e93ed8c94ef37cfabfc12d33e50b5e
rafaelbg <rafaelufrn@gmail.com>
2014-03-05 21:51:13 -0300

    atualizações

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ It uses Opencv2.4.8 for implementation and no more others dependencies.
 Consider citing the related paper to this project:
 Rafael Beserra Gomes, Bruno Motta de Carvalho, Luiz Marcos Garcia Gonçalves, Visual attention guided features selection with foveated images, Neurocomputing, Volume 120, 23 November 2013, Pages 34-44, ISSN 0925-2312, http://dx.doi.org/10.1016/j.neucom.2012.10.033.
 
+This software is under GNU GPL (read LICENSE file for more informations).
+
 Foveation variable summary
 ----------------
 The feature extraction process is done by a sequence of n passes. This differs slight from the original paper. If you want a easy configuration, use one of these values:
--- linearFoveation.h
+++ linearFoveation.h
@@ -1,3 +1,25 @@
+
+/*
+   Copyright (C) 2014, Rafael Beserra <rafaelufrn@gmail.com>
+   If you use this software for academic purposes, consider citing the related paper: Rafael Beserra Gomes, Bruno Motta de Carvalho, Luiz Marcos Garcia Gonçalves, Visual attention guided features selection with foveated images, Neurocomputing, Volume 120, 23 November 2013, Pages 34-44, ISSN 0925-2312, http://dx.doi.org/10.1016/j.neucom.2012.10.033.
+
+   This file is part of foveatedFeatures software.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef LINEAR_FOVEATION
 #define LINEAR_FOVEATION
 
